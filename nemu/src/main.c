377a4b79e243974354ee9fac9e5553fb1d26c985
tracer-ics2018 <tracer@njuics.org>
2018-09-22 13:02:16 -0500

    >  compile 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u4 (2018-08-21) x86_64 GNU/Linux  13:02:16 up  2:28,  3 users,  load average: 0.47, 0.36, 0.25 2619e75453b87373ea75a6f77cdca78fa80b2a4

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -19,6 +19,8 @@ int main(int argc, char *argv[]) {
 	FILE *fp = fopen("input", "r");
 	int cnt = 0;
 	assert(fp != NULL);
+  fscanf(fp, "%u %s\n", &answer, test);
+	printf("%u %s\n", answer, test);
 	while(fscanf(fp, "%u %s\n", &answer, test) == 2){
 		printf("Read something\n");
 		cnt ++;
