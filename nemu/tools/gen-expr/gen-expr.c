844fa4bf0b948ac3d8d83c70ba22a47f15a267d0
tracer-ics2018 <tracer@njuics.org>
2018-09-22 08:16:15 -0500

    >  compile 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u4 (2018-08-21) x86_64 GNU/Linux  08:16:15 up  9:13,  3 users,  load average: 0.32, 0.30, 0.18 f24534ca1a508faa31a0eb66293d153ac3a88f6

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -7,8 +7,48 @@
 
 // this should be enough
 static char buf[65536];
+static int buf_len = 0;
+
+static int choose(int x){
+	if(buf_len > 60000) return 0;
+  int num = (rand())%x;
+	return num;	
+}
+
+static inline void gen_space(){
+	if(buf_len > 60000) return;
+	int num = (rand())%5000;
+	for (int i = 1; i <= num; ++i){
+		buf_len += sprintf(buf, " ");
+	}
+}
+
+static inline void gen_num(){
+	int num = rand();
+  buf_len += sprintf(buf, "%d", num);
+}
+
+static inline void gen(char c){
+	buf_len += sprintf(buf, "%c", c);
+}
+
+static inline void gen_op{
+	int num = choose(4);
+	switch(num){
+		case 0: gen('+'); break;
+		case 1: gen('-'); break;
+		case 2: gen('*'); break;
+		default: gen('/'); break;
+	} 
+}
+
 static inline void gen_rand_expr() {
   buf[0] = '\0';
+	switch (choose(3)){
+		case 0: gen_num(); break;
+		case 1: gen('('); gen_rand_expr(); gen(')');break;
+		default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+	}
 }
 
 static char code_buf[65536];
@@ -27,6 +67,7 @@ int main(int argc, char *argv[]) {
   if (argc > 1) {
     sscanf(argv[1], "%d", &loop);
   }
+	// input the number of loops by command line
   int i;
   for (i = 0; i < loop; i ++) {
     gen_rand_expr();
@@ -39,6 +80,7 @@ int main(int argc, char *argv[]) {
     fclose(fp);
 
     int ret = system("gcc .code.c -o .expr");
+		//system() is used the execute the command line
     if (ret != 0) continue;
 
     fp = popen("./.expr", "r");
