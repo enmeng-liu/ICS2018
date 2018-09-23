ddbcdc4c81193da3d6d15f68d22488f33e3ca608
tracer-ics2018 <tracer@njuics.org>
2018-09-22 23:43:43 -0500

    >  compile 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u4 (2018-08-21) x86_64 GNU/Linux  23:43:43 up  4:16,  4 users,  load average: 0.31, 0.30, 0.22 e11e8d751758c07787ad6943ba2e526fc4ba3a6c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -72,6 +72,7 @@ static bool make_token(char *e) {
 
   nr_token = 0;
 
+	printf("Start make tokens: %s\n", e);
   while (e[position] != '\0') {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
