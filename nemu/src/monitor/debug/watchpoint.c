2e24cdd4f1ae3177c168f391df93575e64e25ea8
tracer-ics2018 <tracer@njuics.org>
2018-09-29 06:04:03 -0500

    >  compile 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u4 (2018-08-21) x86_64 GNU/Linux  06:04:03 up 15:38,  9 users,  load average: 0.28, 0.26, 0.26 8ba1fecae22aac7ffeb4d4ab4f9bbd57123d4aa9

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -154,8 +154,8 @@ void print_wp(){
 	while(temp != NULL){
 		if(temp->change_flag == true){
 			printf("Changed watchpoint NO.%d: %s\n", temp->NO, temp->expr);
-			printf("Old value: (dec)%lld  (hex)0x%llx\n", temp->old_value, temp->old_value);
-			printf("New value: (dec)%lld  (hex)0x%llx\n", temp->new_value,temp->new_value);
+			printf("Old value: (dec)%010lld  (hex)0x%08llx\n", temp->old_value, temp->old_value);
+			printf("New value: (dec)%011lld  (hex)0x%08llx\n", temp->new_value,temp->new_value);
 		}
 		temp = temp->next;
 	}
