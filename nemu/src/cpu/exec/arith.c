eed183224a203c0bb3d34747d6483c2e0e2f6c8d
tracer-ics2018 <tracer@njuics.org>
2018-10-05 04:36:57 -0500

    >  compile 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u4 (2018-08-21) x86_64 GNU/Linux  04:36:57 up 1 day,  6:47, 14 users,  load average: 0.09, 0.12, 0.15 ea67e53f40ceaee0b7153181117441a8977aaba3

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -8,7 +8,7 @@ make_EHelper(add) {
 
 make_EHelper(sub) {
 	rtl_sub(&id_dest->val,&id_dest->val,&id_src->val);
-	Log("Success Sub!");	
+	Log("Success Sub! %x-%x=%x",id_dest->val+id_src->val,id_src->val,id_dest->val);	
   // TODO();
 
   print_asm_template2(sub);
