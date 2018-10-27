9130e906cc2cbde1148f79fa3b0e0418ee1d0ca1
tracer-ics2018 <tracer@njuics.org>
2018-10-27 07:13:35 -0500

    >  run 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  07:13:35 up 11:39, 14 users,  load average: 0.20, 0.19, 0.17 c16265dd676d9fd2c5673763971f5b37503c256a

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,6 +3,40 @@
 
 #ifndef __ISA_NATIVE__
 
+char* itoa(char* dest, int n){
+	/*transfer int into char[]*/
+	//int outn = n;
+	if(n == 0) {
+		dest[0] = '0';
+		dest[1] = '\0';
+		return dest;
+	}
+	
+	int len = 0;
+	int neg = 0;
+	if(n < 0) {
+		neg = 1;
+		len ++;
+		n = -n;
+	}
+	int temp = n;
+	while(temp > 0){
+		len ++;
+		temp /= 10;
+	}
+	
+	for(int i = 0; i < len; ++i){
+		int x = n % 10;
+		dest[len - i - 1] = x + '0';
+		//printf("dest[%d] = %c\n", len-i-1,dest[len-i-1]);
+		n /= 10;
+	}
+	if(neg) dest[0] = '-';
+	dest[len] = '\0';
+	//printf("%d -> %s\n",outn,dest);
+	return dest;
+}
+
 int printf(const char *fmt, ...) {
   return 0;
 }
@@ -11,9 +45,48 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   return 0;
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+
+int sprintf(char* str, const char* format, ...){
+	int len = strlen(format);
+	int i = 0;
+	int in_size = 0;
+	va_list var_arg;
+	va_start(var_arg, format);
+	str[0] = '\0';
+
+	while(i < len){
+		if(format[i] == '%'){
+			int in_num = 0;
+			char in_char[50] = "";
+			switch(format[i+1]){
+				case 'd'://%d 
+					in_num = va_arg(var_arg,int);
+					itoa(in_char,in_num);
+					strcat(str, in_char);
+					in_size += strlen(in_char);
+					break;
+				case 's'://%s
+				 	strcpy(in_char,va_arg(var_arg, char*));
+					strcat(str,in_char);
+					in_size += strlen(in_char);
+					break;
+				default: printf("Not implememted argument type!\n");
+			}
+			i += 2;
+		}
+		else {
+			char temp[2];
+			temp[0] = format[i];
+			temp[1] = '\0';
+			strcat(str, temp);
+			in_size ++;
+			i ++;
+		}
+	}
+		va_end(var_arg);
+		return in_size;
 }
+
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   return 0;
