771a3dbcf7213d4e97ff95211d991a6b1e710f33
tracer-ics2018 <tracer@njuics.org>
2018-11-03 08:00:04 -0500

    >  compile 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  08:00:04 up  4:36, 12 users,  load average: 0.16, 0.22, 0.26 ffe3b53683f5f16f65a7d936fd63574be623e5f9

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -3,6 +3,10 @@
 #include <amdev.h>
 #include <klib.h>
 
+#define SCREEN_PORT 0x100
+#define SCREEN_H 300
+#define SCREEN_W 400
+
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
@@ -21,6 +25,9 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
+			int i;
+			int size = screen_width() * screen_height();
+			for(i = 0; i < size; ++i) fb[i] = i;
 
       if (ctl->sync) {
         // do nothing, hardware syncs.
