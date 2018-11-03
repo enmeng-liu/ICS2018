7b50efd680ad6248b0053e7920107092fd5241d8
tracer-ics2018 <tracer@njuics.org>
2018-11-03 09:33:37 -0500

    >  compile 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  09:33:37 up  5:09, 13 users,  load average: 0.40, 0.30, 0.22 8b05e6eb1f62de22614c337d78f328964e407d4f

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -7,6 +7,11 @@
 #define SCREEN_H 300
 #define SCREEN_W 400
 
+int min(int a, int b){
+	if(a <= b) return a;
+	else return b;
+}
+
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
@@ -25,10 +30,17 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
-			int i;
+			/*int i;
 			int size = screen_width() * screen_height();
 			for(i = 0; i < size; ++i) fb[i] = i;
-
+			*/
+			int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+			uint32_t *pixels = ctl->pixels;
+			int cp_bytes = sizeof(uint32_t) * min(w, SCREEN_W - x);
+			for(int j = 0; j < h  && y + j < SCREEN_H; ++j){
+				memcpy(&fb[(y + j) * SCREEN_W + x], pixels, cp_bytes);
+				pixels += w;
+			}
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
