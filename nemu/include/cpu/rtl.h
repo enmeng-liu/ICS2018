0fcfbbde067d72d3840b15e16ccfc88e3c07db49
tracer-ics2018 <tracer@njuics.org>
2018-11-03 22:18:53 -0500

    >  compile 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  22:18:53 up 15:55, 13 users,  load average: 0.43, 0.16, 0.11 55f5fc97379f7742940d610fd7eaaf0d158f5aae

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -77,7 +77,7 @@ static inline void interpret_rtl_idiv64_r(rtlreg_t* dest,
   *dest = dividend % divisor;
 }
 
-static  void interpret_rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int len) {
+static inline void interpret_rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int len) {
   *dest = vaddr_read(*addr, len);
 }
 
