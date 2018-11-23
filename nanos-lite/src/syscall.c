6bfa63d243ed434870d231ba810ea5071dc12b5b
tracer-ics2018 <tracer@njuics.org>
2018-11-23 05:43:09 -0600

    >  run 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  05:43:09 up  7:15, 18 users,  load average: 0.19, 0.19, 0.17 6874e1e5aa727c33a38857d885e77052d7c85a90

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,11 +3,15 @@
 
 extern _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
-  a[0] = c->GPR1;
+  a[0] = c->GPR1;	//type 
+	a[1] = c->GPR2;	//a0 parameter 1
+	a[2] = c->GPR3;
+	a[3] = c->GPR4;
 
   switch (a[0]) {
+		case SYS_yield: _yield(); c->GPRx = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
-  return NULL;
+  return c;
 }
