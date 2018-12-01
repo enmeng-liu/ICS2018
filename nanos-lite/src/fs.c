7c0c4f1ec0085207d324b02e7c353f483168d95c
tracer-ics2018 <tracer@njuics.org>
2018-12-01 04:12:13 -0600

    >  run 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  04:12:13 up  4:57, 10 users,  load average: 0.34, 0.26, 0.16 9cc4b1d4481fd3bda579326e60cf57d2f743d

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -97,7 +97,7 @@ extern off_t fs_lseek(int fd, off_t offset, int whence){
 		case SEEK_SET: file_table[fd].open_offset = offset; break;
 		case SEEK_CUR: file_table[fd].open_offset += offset; break;
 		case SEEK_END: file_table[fd].open_offset = file_table[fd].size + offset; break;
-		default: panic("Unknown whence!");
+		default: return -1;
 	}
 	Log("fs_lseek: new offset = %d", file_table[fd].open_offset);
 	if(file_table[fd].open_offset > file_table[fd].size)
