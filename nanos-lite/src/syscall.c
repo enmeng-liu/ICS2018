686f807ca5648d70d7d4337d5f524648411a216d
tracer-ics2018 <tracer@njuics.org>
2018-12-01 04:22:56 -0600

    >  run 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  04:22:56 up  5:08,  9 users,  load average: 0.45, 0.28, 0.20 bb4509e23431d8cf23c07b4bfe592ad14fa26ae0

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,6 +9,7 @@ int fs_open(const char* pathname, int flags, int mode);
 ssize_t fs_read(int fd, void* buf, size_t len);
 ssize_t fs_close(int fd);
 ssize_t fs_lseek(int fd, off_t offset, int whence);
+ssize_t fs_write(int fd, const void* buf, size_t len);
 
 extern _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
@@ -25,15 +26,13 @@ extern _Context* do_syscall(_Context *c) {
 		case SYS_exit:  Log("call sys_exit!");
 										_halt(a[2]);
 									  break;
-		case SYS_write: //Log("call sys_write!");
-										//int fd = (int)a[1];
-										//char* buf = (char*)a[2];
-										//size_t count = (size_t)a[3];
-										if(a[1] == 1 || a[1] == 2){
+		case SYS_write: Log("call sys_write!");
+										/*if(a[1] == 1 || a[1] == 2){
 											char* buf = (char*)a[2];
 											for(int i = 0; i < a[3]; ++i) _putc(buf[i]);
 										}
-										c->GPRx = (size_t)a[3];
+										c->GPRx = (size_t)a[3];*/
+										c->GPRx = (ssize_t)fs_write((int)a[1], (const void*)a[2], (size_t)a[3]);
 										break;
 		case SYS_brk:		Log("call sys_brk!");
 										_heap.start = &_end;
@@ -41,16 +40,16 @@ extern _Context* do_syscall(_Context *c) {
 										c->GPRx = 0;
 										break;
 		case SYS_read: 	Log("call sys_read!");
-									 	c->GPRx = fs_read(a[1], (void*)a[2], a[3]);
+									 	c->GPRx = (ssize_t)fs_read((int)a[1], (void*)a[2], (size_t)a[3]);
 										break;
 		case SYS_open: 	Log("call sys_open!");
-									 	c->GPRx = fs_open((void*)a[1], a[2], a[3]);
+									 	c->GPRx = (ssize_t)fs_open((const char*)a[1], (int)a[2], (int)a[3]);
 									 	break;
 		case SYS_close: Log("call sys_close!");
-										c->GPRx = fs_close(a[1]);
+										c->GPRx = (ssize_t)fs_close((int)a[1]);
 									  break;
 		case SYS_lseek: Log("call sys_lseek!");
-										c->GPRx = fs_lseek(a[1], a[2], a[3]);
+										c->GPRx = (off_t)fs_lseek((int)a[1], (off_t)a[2], (int)a[3]);
 										break;	
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
