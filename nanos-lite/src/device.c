47738e8d530a810eb7e9ccf0191f35ecfc28372e
tracer-ics2018 <tracer@njuics.org>
2018-12-02 07:01:59 -0600

    >  run 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  07:01:59 up 14:14, 22 users,  load average: 0.54, 0.34, 0.23 93d08520fb1501eff51af1dd766ea43873499991

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -25,12 +25,12 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 	char temp[128]; 
 	if(keytemp != 0) {
 		Log("get key: %d", keytemp ^ 0x8000);
-		if(keytemp & 0x8000){
-			sprintf(temp, "ku %s\n", keyname[keytemp ^ 0x8000]);
+		if((keytemp & 0x8000) != 0){
+			sprintf(temp, "kd %s\n", keyname[keytemp ^ 0x8000]);
 			strncpy(buf, temp, len);
 		}
 		else {
-			sprintf(temp, "kd %s\n", keyname[keytemp ^ 0x8000]);
+			sprintf(temp, "ku %s\n", keyname[keytemp ^ 0x8000]);
 			strncpy(buf, temp, len);
 		}
 	}
