f76ebdd612074e23ab91f20db09e4cad4690a2b3
tracer-ics2018 <tracer@njuics.org>
2018-12-15 07:56:17 -0600

    >  run 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  07:56:17 up  6:11, 25 users,  load average: 0.05, 0.26, 0.25 b975edc5d929fc718418285f8936818165f72ed4

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -67,7 +67,7 @@ _Context *_kcontext(_Area stack, void (*entry)(void *), void *arg) {
 	_Context cont;
 	cont.cs = 8;
 	cont.eip = (uintptr_t)(entry);
-	cont.ebp = (int)(stack.start + 4);
+	cont.ebp = (int)(stack.start);
 	cont.edi = cont.esi = cont.esp = cont.ebx = cont.ecx = cont.eax = 0;
 	cont.irq = cont.eip = cont.eflags = 0;
 	//how to initialize cont?
