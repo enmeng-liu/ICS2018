7a124b76def8031f8e9381085219242781bdd9ff
tracer-ics2018 <tracer@njuics.org>
2018-12-16 01:46:12 -0600

    >  compile 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  01:46:12 up  9:52, 29 users,  load average: 0.26, 0.33, 0.33 249bc8058be23a3d32699a48f364447086b34237

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -21,15 +21,25 @@ make_EHelper(lidt) {
 make_EHelper(mov_r2cr) {
   //TODO();
 	Log("id_dest = %d", id_dest->reg);
-	operand_write(id_dest, &id_src->val);
+	//operand_write(id_dest, &id_src->val);
+	switch(id_dest->reg) {
+		case 0: cpu.cr0 = id_src->val; break;
+		case 3: cpu.cr3 = id_src->val; break;
+		default: panic("Invalid control reg!");
+	}
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
   //TODO();
 	Log("id_src = %d", id_src->reg);
+	switch(id_src->reg) {
+		case 0: operand_write(id_dest, &cpu.cr0);
+		case 3:	operand_write(id_dest, &cpu.cr3);
+		default: panic("Invalid control reg!");
+	}
 	assert(0);
-	operand_write(id_dest, &id_src->val);
+	//operand_write(id_dest, &id_src->val);
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
 #if defined(DIFF_TEST)
