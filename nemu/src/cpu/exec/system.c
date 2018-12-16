ce6e944fbd17449aa9074e98f7bb333b7d1d461d
tracer-ics2018 <tracer@njuics.org>
2018-12-16 01:48:27 -0600

    >  compile 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  01:48:27 up  9:54, 29 users,  load average: 0.56, 0.42, 0.37 bd48aa7e931aaf98df925c7e2d028a108182798f

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -20,24 +20,28 @@ make_EHelper(lidt) {
 
 make_EHelper(mov_r2cr) {
   //TODO();
-	Log("id_dest = %d", id_dest->reg);
+	Log("id_dest = %d", id_dest->reg);;
+	Log("id_src->val = %d", id_src->val);
 	//operand_write(id_dest, &id_src->val);
 	switch(id_dest->reg) {
 		case 0: cpu.cr0 = id_src->val; break;
 		case 3: cpu.cr3 = id_src->val; break;
 		default: panic("Invalid control reg!");
 	}
+	Log("cr0 = %d, cr3 = %d", cpu.cr0, cpu.cr3);
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
   //TODO();
 	Log("id_src = %d", id_src->reg);
+	Log("id-dest->val = %d", id_dest->val);
 	switch(id_src->reg) {
 		case 0: operand_write(id_dest, &cpu.cr0); break;
 		case 3:	operand_write(id_dest, &cpu.cr3); break;
 		default: panic("Invalid control reg!");
 	}
+	Log("cr0 = %d, cr3 = %d", cpu.cr0, cpu.cr3);
 	assert(0);
 	//operand_write(id_dest, &id_src->val);
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
