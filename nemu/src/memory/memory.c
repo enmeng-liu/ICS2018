6034c93560d187a980432c014490c60f8eeaa8f7
tracer-ics2018 <tracer@njuics.org>
2018-12-16 03:23:45 -0600

    >  compile 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  03:23:45 up 11:30, 29 users,  load average: 0.22, 0.15, 0.14 83bcc88af279dddf8d7e3fae81bca1dc8d4fa64

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -2,6 +2,9 @@
 #include "device/mmio.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
+#define VMEM_SIZE 0xffffffff
+#define PRESENT(x) (x & 0x00000001)
+#define HIGH20(x) ((x & 0xfffff000) >> 12)
 
 #define pmem_rw(addr, type) *(type *)({\
     Assert(addr < PMEM_SIZE, "physical address(0x%08x) is out of bound", addr); \
@@ -9,6 +12,19 @@
     })
 
 uint8_t pmem[PMEM_SIZE];
+
+paddr_t page_translate(vaddr_t addr) {
+	uint32_t dir = addr >> 22;
+	uint32_t page = (addr & 0x003ff000) >> 12;
+	uint32_t offset = addr & 0x00000fff;
+	uint32_t page_dir_addr = HIGH20(cpu.cr3);
+	uint32_t page_addr = paddr_read(page_dir_addr + 4 * dir, 4);
+	assert(PRESENT(page_addr) == 1);
+	uint32_t page_num = paddr_read(page_addr + 4 * page, 4);
+	assert(PRESENT(page_num) == 1);
+  paddr_t paddr = HIGH20(page_num) + offset;
+	return paddr;	
+}
 
 /* Memory accessing interfaces */
 
@@ -31,9 +47,21 @@ void paddr_write(paddr_t addr, uint32_t data, int len) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+	if(addr + len - 1 > VMEM_SIZE) {
+		assert(0);
+	}// data cross the page boundary
+	else {
+		paddr_t paddr = page_translate(addr);
+  	return paddr_read(paddr, len);
+	}
 }
 
 void vaddr_write(vaddr_t addr, uint32_t data, int len) {
-  paddr_write(addr, data, len);
+	if(addr + len - 1 > VMEM_SIZE) {
+		assert(0);
+	}
+	else {
+		paddr_t paddr = page_translate(addr);			
+ 	  paddr_write(paddr, data, len);
+	}
 }
