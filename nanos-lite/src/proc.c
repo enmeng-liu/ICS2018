7e5bd0b4228f9d7d494dc64c619fad8e49916a87
tracer-ics2018 <tracer@njuics.org>
2018-12-15 19:38:25 -0600

    >  run 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  19:38:25 up  7:08, 24 users,  load average: 0.50, 0.54, 0.34 aae1d5dab82c1efdec49f4b1b7f34d2c6b934ab

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,7 @@ void hello_fun(void *arg) {
 void init_proc() {
 	//naive_uload(NULL, "/bin/init");
 	context_kload(&pcb[0], (void*)hello_fun);
-	context_uload(&pcb[1], "/bin/init");
+	context_uload(&pcb[1], "/bin/hello");
 	switch_boot_pcb();
 }
 
