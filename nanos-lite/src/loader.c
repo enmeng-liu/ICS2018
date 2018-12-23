3409e7043c1dc1d6eecf4607168da5773edb9515
tracer-ics2018 <tracer@njuics.org>
2018-12-23 02:38:15 -0600

    >  run 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  02:38:15 up 50 min,  8 users,  load average: 0.03, 0.12, 0.11 dc81506c66cd89c2fc06b869f40798c1f5a2baa8

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -48,7 +48,7 @@ void context_kload(PCB *pcb, void *entry) {
 void context_uload(PCB *pcb, const char *filename) {
 	Log("pcb->as.ptr=%p", pcb->as.ptr);
 	Log("pcb->cp.prot=%p", pcb->cp->prot);
-	_switch(pcb->cp);
+	_protect(&(pcb->as));
   uintptr_t entry = loader(pcb, filename);
 
   _Area stack;
