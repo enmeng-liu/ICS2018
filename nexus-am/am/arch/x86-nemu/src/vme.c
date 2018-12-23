e708560b6c125903d17b7cbf3e2f1f3aca85ae0a
tracer-ics2018 <tracer@njuics.org>
2018-12-23 03:14:17 -0600

    >  run 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  03:14:17 up  1:26,  7 users,  load average: 0.45, 0.30, 0.16 6aaba689bfa6be816a0125e61948d28316eb433a

--- nexus-am/am/arch/x86-nemu/src/vme.c
+++ nexus-am/am/arch/x86-nemu/src/vme.c
@@ -99,8 +99,8 @@ int _map(_Protect *p, void *va, void *pa, int mode) {
 	uint32_t page = PTX(va);
 	void* pt_addr = (void*)((map_pde >> 12) << 12); //get page table addr
 	PTE* map_pte_ptr = (PTE*)(pt_addr + page * 4);
-	assert(0);
 	*map_pte_ptr = (paddr >> 12) << 12; //fill in physical page number
+	assert(0);
 	*map_pte_ptr |= PTE_P; // set present
 	if((mode & PTE_P) == 0) *map_pte_ptr = 0; // make mapping invalid
   return 0;
