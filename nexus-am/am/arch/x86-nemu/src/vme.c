8551d8abed3247aa6228403eb42086bd501509c0
tracer-ics2018 <tracer@njuics.org>
2018-12-23 03:12:24 -0600

    >  run 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  03:12:24 up  1:24,  7 users,  load average: 0.29, 0.18, 0.11 a3174e342aff079d595f33e38e014034a1dcfa69

--- nexus-am/am/arch/x86-nemu/src/vme.c
+++ nexus-am/am/arch/x86-nemu/src/vme.c
@@ -88,11 +88,12 @@ int _map(_Protect *p, void *va, void *pa, int mode) {
 	if((map_pde & PTE_P) == 0) {
 		printf("create new PDE\n");
 		PDE new_page_addr = (PDE)pgalloc_usr(1);
-		printf("new PDE is at %p", new_page_addr);
+		printf("new PDE is at %p\n", new_page_addr);
 	 	PDE* map_pde_ptr = p->ptr + 4 * dir;
+		assert(0);
 		*map_pde_ptr = (new_page_addr >> 12) << 12;
 	 	*map_pde_ptr |= PTE_P;	
-		printf("now map_pde=%p",*map_pde_ptr);
+		printf("now map_pde=%p\n",*map_pde_ptr);
 		//create a new page
 	}
 
