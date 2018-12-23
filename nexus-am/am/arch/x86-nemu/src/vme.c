77930c12df4de20bd1dbbe56c3baae86e3d83081
tracer-ics2018 <tracer@njuics.org>
2018-12-23 03:13:16 -0600

    >  run 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  03:13:16 up  1:25,  7 users,  load average: 0.52, 0.26, 0.14 7532e572999aa33c9942c0da868880a06231d05

--- nexus-am/am/arch/x86-nemu/src/vme.c
+++ nexus-am/am/arch/x86-nemu/src/vme.c
@@ -92,8 +92,8 @@ int _map(_Protect *p, void *va, void *pa, int mode) {
 	 	PDE* map_pde_ptr = p->ptr + 4 * dir;
 		*map_pde_ptr = (new_page_addr >> 12) << 12;
 	 	*map_pde_ptr |= PTE_P;	
-		assert(0);
 		printf("now map_pde=%p\n",*map_pde_ptr);
+		assert(0);
 		//create a new page
 	}
 
