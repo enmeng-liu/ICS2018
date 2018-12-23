f97fac67861554ae8bd6e8d339264f8aa166d588
tracer-ics2018 <tracer@njuics.org>
2018-12-23 03:17:09 -0600

    >  run 171860013 mengzelev Linux mengzelev 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  03:17:09 up  1:29,  7 users,  load average: 0.23, 0.32, 0.19 42aca5b38910bd4573414e81066d4a1c449a454

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -24,10 +24,11 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 	//int pgnum = filesz / PGSIZE;
 	for(int i = 0; i < filesz/PGSIZE; ++i) {
 		void* pa = new_page(1);
+		Log("pa = %p", pa);
 		//Log("create %d new page(s)", filesz/PGSIZE);
 		//Log("pcb->as.ptr=%p",pcb->as.ptr);
 		_map(&(pcb->as), (void*)(DEFAULT_ENTRY + PGSIZE), pa, 1);
-		Log("map va=0x%p to pa=0x%x", (void*)DEFAULT_ENTRY, pa);
+		//Log("map va=0x%p to pa=0x%x", (void*)DEFAULT_ENTRY, pa);
 	}
   fs_read(fd, (void*)DEFAULT_ENTRY, fs_filesz(fd));
 	fs_close(fd);
